a66c396c6ac6f836180b7abb6e295b5960050984
realerhd1 <137207458+realerhd1@users.noreply.github.com>
2025-03-24 23:28:02 +0100

    Delete install-yt-dlp.sh

--- install-yt-dlp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Lade die Linux-Version von yt-dlp herunter (damit es auf Render funktioniert)
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o yt-dlp
-chmod +x yt-dlp
-mv yt-dlp /usr/local/bin/yt-dlp
