4977f2c98bc2b8e2cf113ccef9c44c37657188db
realerhd1 <137207458+realerhd1@users.noreply.github.com>
2025-03-24 23:31:57 +0100

    Update install-yt-dlp.sh

--- install-yt-dlp.sh
+++ install-yt-dlp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
-chmod +x yt-dlp
-mv yt-dlp /usr/local/bin/yt-dlp
+
+mkdir -p ./bin
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o ./bin/yt-dlp
+chmod +x ./bin/yt-dlp
