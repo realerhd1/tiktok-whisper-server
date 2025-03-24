a207a86619683c025995f03f3c34e42115132565
realerhd1 <137207458+realerhd1@users.noreply.github.com>
2025-03-24 23:00:37 +0100

    Update install-yt-dlp.sh

--- install-yt-dlp.sh
+++ install-yt-dlp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+
+# Lade die Linux-Version von yt-dlp herunter (damit es auf Render funktioniert)
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o yt-dlp
 chmod +x yt-dlp
 mv yt-dlp /usr/local/bin/yt-dlp
